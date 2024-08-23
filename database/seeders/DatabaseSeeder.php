1411dd8860aa7fdc5361e3e1568d10baf5a81315
rodlez <xavier.rodlez@gmail.com>
2024-08-23 12:05:32 +0200

    Create the tables Customer and Invoice and populate them

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Customer;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,6 +19,11 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        // call the seeder to populate the DB
+        $this->call([
+            CustomerSeeder::class
         ]);
     }
 }
